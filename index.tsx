09633f1ce2889375cc18516e69690400db3b39ae
Sena <104799682+gabrsena@users.noreply.github.com>
2026-01-16 14:34:17 -0300

    feat: Add animated section to landing page
    Introduces a new animated section to the landing page, enhancing visual appeal and user engagement. The section is designed to fade in and scale up as the user scrolls into view.

--- index.tsx
+++ index.tsx
@@ -790,6 +790,7 @@ const SafetySection: React.FC = () => {
              </ul>
           </div>
         </motion.div>
+
         <motion.div 
            initial={{ opacity: 0, scale: 0.8 }}
            whileInView={{ opacity: 1, scale: 1 }}
